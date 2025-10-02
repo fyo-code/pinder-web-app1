30ada22b7e123816210b5937f3bdf8b7abf0162a
fyo-code <fyodor200523@gmail.com>
2025-10-02 19:33:46 +0200

    add globals.css next to layout & fix import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,16 @@
 // src/app/layout.tsx
-import './globals.css'
-import type { Metadata } from 'next'
+import './globals.css';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Pinder',
-  description: 'Find the perfect match for your pet.',
-}
+  description: 'Find the perfect match for your pet',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-brand-grayBg text-gray-800 antialiased">
-        {/* Page content */}
-        <div className="mx-auto max-w-5xl px-4 pb-24">{children}</div>
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
